d662664cc2099e80899a3a7413b76f81e8bd1ed7
Diego Irreño <dirreno@unal.edu.co>
2022-10-17 23:49:37 -0500

    Historia academica vista

--- src/Pages/AcademicRecord/AcademicRecord.js
+++ src/Pages/AcademicRecord/AcademicRecord.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import "./AcademicRecord.css"
 
 const AcademicRecord = () =>{
+    const color = "green"
+
     return (
         <div className="record">
-            <h1>Historia Académica</h1>
+            <div>
+                <h1>Historia Académica</h1>
+                <h2>Ingeníeria de sistemas y computación</h2>
+                <b>Estado: Abierto <span style={{color}}>●</span> </b>
+                <br/>
+                <b>Facultad de Ingeniería, Sede Bogotá</b>
+            </div>
+            <div className="r2">
+                <div className="sr">
+                    <h2 className="srt">Materias cursadas</h2>
+                </div>
+                <div className="sr">
+                    <h2 className="srt">Materias pendientes</h2>
+                </div>
+                <div className="small">
+                    <h2 className="sm">PAPA</h2>
+                </div>
+                <div className="small">
+                    <h2 className="sm">PAPI</h2>
+                </div>
+            </div>
+
         </div>
     );
 }
