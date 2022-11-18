4e3328190cba881de8c1f57bc5a7ac224c1da46f
Diego Irreño <dirreno@unal.edu.co>
2022-11-18 15:54:05 -0500

    fix

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,16 +2,9 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        '/auth',
-        createProxyMiddleware({
-            target: 'http://localhost:8090',
-            changeOrigin:true,
-        })
-    );
-    app.use(
         '/graphql',
         createProxyMiddleware({
-            target: 'http://localhost:8090',
+            target: 'http://10.44.14.187',
             changeOrigin:true,
         })
     );
